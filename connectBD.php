bf633a7e8b7483eba1bba151f26b8949d818647e
joaovitorcidralv <119749953+joaovitorcidralv@users.noreply.github.com>
2023-04-24 17:17:48 -0300

    Add files via upload

---
+++ connectBD.php
@@ -0,0 +1,11 @@
+<?php
+global $servername ;
+global $username;
+global $password;
+global $database;
+
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$database = "GrowhealthY";
+?>
