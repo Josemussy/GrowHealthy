b469444b976d11571c69e3bc5baf951d11c3797e
joaovitorcidralv <119749953+joaovitorcidralv@users.noreply.github.com>
2023-05-02 16:09:54 -0300

    Update bd_growth.sql

--- AcompanhaFit/bd/bd_growth.sql
+++ AcompanhaFit/bd/bd_growth.sql
@@ -14,14 +14,14 @@ CREATE TABLE `Personal` (
   `ID_Personal` int(11) NOT NULL,
   `Nome` varchar(100) NOT NULL,
   `Gênero` varchar(100) COLLATE latin1_general_ci NOT NULL,
-  `Cref` varchar(15) NOT NULL,
+  `CREF` varchar(15) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `Nutricionista` (
   `ID_Nutricionista` int(11) NOT NULL,
   `Nome` varchar(100) NOT NULL,
   `Gênero` varchar(100) COLLATE latin1_general_ci NOT NULL,
-  `Crn` varchar(15) NOT NULL,
+  `CRN` varchar(15) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `Aluno` (
@@ -33,3 +33,31 @@ CREATE TABLE `Aluno` (
   'Peso' int(200) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
+ALTER TABLE `Personal`
+  ADD PRIMARY KEY (`ID_Personal`),
+  ADD UNIQUE KEY `UN_CREF` (`CREF`),
+  
+ALTER TABLE `Nutricionista`
+  ADD PRIMARY KEY (`ID_Nutricionista`),
+  ADD UNIQUE KEY `UN_CRN` (`CRN`),
+  
+ALTER TABLE `Aluno`
+  ADD PRIMARY KEY (`ID_Aluno`),
+  
+ALTER TABLE `Usuario`
+  ADD PRIMARY KEY (`ID_Usuario`);
+  
+ALTER TABLE `Personal`
+  MODIFY `ID_Personal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  
+ALTER TABLE `Nutricionista`
+  MODIFY `ID_Nutricionista` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  
+ALTER TABLE `Aluno`
+  MODIFY `ID_Aluno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  
+ALTER TABLE `Usuario`
+  MODIFY `ID_Usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  
+  
+  
