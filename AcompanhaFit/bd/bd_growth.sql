ad27ad89bf178ca8d68a8e11399f51dc3cf73804
joaovitorcidralv <119749953+joaovitorcidralv@users.noreply.github.com>
2023-05-02 15:59:42 -0300

    Update bd_growth.sql

--- AcompanhaFit/bd/bd_growth.sql
+++ AcompanhaFit/bd/bd_growth.sql
@@ -2,13 +2,25 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+CREATE TABLE `medico` (
+  `ID_Medico` int(11) NOT NULL,
+  `CRM` varchar(15) NOT NULL,
+  `Nome` varchar(100) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `Usuario` (
+  `ID_Usuario` int(11) NOT NULL,
+  `Nome` varchar(100) COLLATE latin1_general_ci NOT NULL,
+  `Celular` varchar(20) COLLATE latin1_general_ci NOT NULL,
+  `Login` varchar(50) COLLATE latin1_general_ci DEFAULT NULL,
+  `Senha` varchar(40) COLLATE latin1_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
 CREATE TABLE `Personal` (
   `ID_Personal` int(11) NOT NULL,
   `Nome` varchar(100) NOT NULL,
   `Gênero` varchar(100) COLLATE latin1_general_ci NOT NULL,
   `Cref` varchar(15) NOT NULL,
-  `Login` varchar(50) COLLATE latin1_general_ci DEFAULT NULL,
-  `Senha` varchar(40) COLLATE latin1_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `Nutricionista` (
@@ -16,8 +28,6 @@ CREATE TABLE `Nutricionista` (
   `Nome` varchar(100) NOT NULL,
   `Gênero` varchar(100) COLLATE latin1_general_ci NOT NULL,
   `Crn` varchar(15) NOT NULL,
-  `Login` varchar(50) COLLATE latin1_general_ci DEFAULT NULL,
-  `Senha` varchar(40) COLLATE latin1_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `Aluno` (
@@ -27,7 +37,5 @@ CREATE TABLE `Aluno` (
   `Email` varchar(50) COLLATE latin1_general_ci DEFAULT NULL,
   'Altura' double(11) NOT NULL,
   'Peso' int(200) NOT NULL,
-  `Login` varchar(50) COLLATE latin1_general_ci DEFAULT NULL,
-  `Senha` varchar(40) COLLATE latin1_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
