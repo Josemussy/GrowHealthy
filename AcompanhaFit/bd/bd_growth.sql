fd65bca9513b1d160ee17339a1a7ff7779d12277
joaovitorcidralv <119749953+joaovitorcidralv@users.noreply.github.com>
2023-05-02 15:40:20 -0300

    Update bd_growth.sql

--- AcompanhaFit/bd/bd_growth.sql
+++ AcompanhaFit/bd/bd_growth.sql
@@ -6,7 +6,8 @@ CREATE TABLE `Personal` (
   `ID_Personal` int(11) NOT NULL,
   `Nome` varchar(100) NOT NULL,
   `Gênero` varchar(100) COLLATE latin1_general_ci NOT NULL,
-  `CREF` varchar(15) NOT NULL,
+  `Cref` varchar(15) NOT NULL,
+  `Login` varchar(50) COLLATE latin1_general_ci DEFAULT NULL,
   `Senha` varchar(40) COLLATE latin1_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -14,7 +15,8 @@ CREATE TABLE `Nutricionista` (
   `ID_Nutricionista` int(11) NOT NULL,
   `Nome` varchar(100) NOT NULL,
   `Gênero` varchar(100) COLLATE latin1_general_ci NOT NULL,
-  `CRN` varchar(15) NOT NULL,
+  `Crn` varchar(15) NOT NULL,
+  `Login` varchar(50) COLLATE latin1_general_ci DEFAULT NULL,
   `Senha` varchar(40) COLLATE latin1_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -25,6 +27,7 @@ CREATE TABLE `Aluno` (
   `Email` varchar(50) COLLATE latin1_general_ci DEFAULT NULL,
   'Altura' double(11) NOT NULL,
   'Peso' int(200) NOT NULL,
+  `Login` varchar(50) COLLATE latin1_general_ci DEFAULT NULL,
   `Senha` varchar(40) COLLATE latin1_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
