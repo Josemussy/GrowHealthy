0e22f465ad2857e5b8f2bebad65075a09ecc5f6c
Felipe <107360437+felipesphair@users.noreply.github.com>
2023-05-03 22:37:27 -0300

    Add files via upload
    index modificado

---
+++ GrowHealthy-desenvolvimento/bd/conectaBD.php
@@ -0,0 +1,24 @@
+<!-------------------------------------------------------------------------------
+Oficina Desenvolvimento Web
+PUCPR
+
+CONECTABD.PHP - deve ser incluído em todos os arquivos PHP que precisam de acesso à BD
+
+Profa. Cristina V. P. B. Souza
+Março/2023
+---------------------------------------------------------------------------------->
+<?php
+global $servername ;
+global $username;
+global $password;
+global $database;
+
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$database = "I_Ensino";
+?>
+
+
+
+
