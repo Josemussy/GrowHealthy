06a7775ecf69583bbe00f6e2081428f565be025f
joaovitorcidralv <119749953+joaovitorcidralv@users.noreply.github.com>
2023-05-13 18:31:22 -0300

    Update menu.php

--- GrowHealthy-desenvolvimento/geral/menu.php
+++ GrowHealthy-desenvolvimento/geral/menu.php
@@ -1,4 +1,10 @@
-vicon
+<!-------------------------------------------------------------------------------
+    Desenvolvimento Web
+    PUCPR
+    Profa. Cristina V. P. B. Souza
+    Março/2023
+---------------------------------------------------------------------------------->
+<!-- menu.php -->
 
 	<?php
 		require('verifica_login.php');
@@ -34,9 +40,7 @@ vicon
 		</div>
 		<div class="w3-bar w3-theme w3-large" style="z-index:-1">
 			<a class="w3-bar-item w3-button w3-left w3-hide-large w3-hover-light-gray w3-large w3-theme w3-padding-16" href="javascript:void(0)" onclick="w3_open()">☰</a>
-			<a class="w3-bar-item w3-button w3-hide-medium w3-hide-small w3-hover-light-gray w3-padding-16" href="professor.php" onclick="w3_show_nav('menuProf')">Professores</a>
-			<a class="w3-bar-item w3-button w3-hide-medium w3-hide-small w3-hover-light-gray w3-padding-16" href="disciplina.php" onclick="w3_show_nav('menuDisc')">Disciplinas</a>
-			<a class="w3-bar-item w3-button w3-hide-medium w3-hide-small w3-hover-light-gray w3-padding-16" href="turma.php" onclick="w3_show_nav('menuTurma')">Turmas</a>
+			<a class="w3-bar-item w3-button w3-hide-medium w3-hide-small w3-hover-light-gray w3-padding-16" href="professor.php" onclick="w3_show_nav('menuProf')">Personais</a>
 		</div>
 	</div>
 
@@ -46,38 +50,18 @@ vicon
 	<div class="w3-sidebar w3-bar-block w3-collapse w3-animate-left" style="z-index:3;width:270px" id="mySidebar" >
 		<div class="w3-bar w3-hide-large w3-large">
 			<a href="javascript:void(0)" onclick="w3_show_nav('menuProf')"
-			   class="w3-bar-item w3-button w3-theme w3-hover-light-gray w3-padding-16" style="width:50%">Professores</a>
-			<a href="javascript:void(0)" onclick="w3_show_nav('menuDisc')"
-			   class="w3-bar-item w3-button w3-theme w3-hover-light-gray w3-padding-16" style="width:50%">Disciplinas</a>
-			<a href="javascript:void(0)" onclick="w3_show_nav('menuTurma')"
-			   class="w3-bar-item w3-button w3-theme w3-hover-light-gray w3-padding-16" style="width:50%">Turma</a>
+			   class="w3-bar-item w3-button w3-theme w3-hover-light-gray w3-padding-16" style="width:50%">Personais</a>
 			   
 		</div>
 		<a href="javascript:void(0)" onclick="w3_close()" class="w3-button w3-right w3-xlarge w3-hide-large"
 		   title="Close Menu">x</a>
 		<div id="menuProf" class="myMenu">
 			<div class="w3-container">
-				<h3>Menu Professores</h3>
+				<h3>Menu Personais</h3>
 			</div>
-			<a class="w3-bar-item w3-button" href="profListar.php">Relação de Professores</a>
-			<a class="w3-bar-item w3-button" href="profIncluir.php">Contratação de Professores </a>
+			<a class="w3-bar-item w3-button" href="profListar.php">Relação de Personais</a>
+			<a class="w3-bar-item w3-button" href="profIncluir.php">Contratação de Personais </a>
 
-
-		</div>
-		<div id="menuDisc" class="myMenu" >
-			<div class="w3-container">
-				<h3>Menu Disciplinas</h3>
-			</div>
-			<a class="w3-bar-item w3-button" href='discListar.php'>Relação de Disciplinas</a>
-			<a class="w3-bar-item w3-button" href='discIncluir.php'>Criar Nova Disciplina</a>
-
-		</div>
-		<div id="menuTurma" class="myMenu" >
-			<div class="w3-container">
-				<h3>Menu Turmas</h3>
-			</div>
-			<a class="w3-bar-item w3-button" href='turmaListar.php'>Relação de Turmas</a>
-			<a class="w3-bar-item w3-button" href='turmaIncluir.php'>Criar Nova Turma</a>
 
 		</div>
 	</div>
