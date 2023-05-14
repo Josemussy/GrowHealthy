6425a6d842b38085cf1aaf9ec47f5b79d205e285
joaovitorcidralv <119749953+joaovitorcidralv@users.noreply.github.com>
2023-05-14 16:17:46 -0300

    Update cadastro.php

--- GrowHealthy-desenvolvimento/cadastro.php
+++ GrowHealthy-desenvolvimento/cadastro.php
@@ -9,7 +9,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>IE - Instituição de Ensino</title>
+    <title>GrowHealthy</title>
     <link rel="icon" type="image/png" href="imagens/favicon.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
