cecf5adcd9cb4ae910249912082eb93d61346b5b
joaovitorcidralv <119749953+joaovitorcidralv@users.noreply.github.com>
2023-05-14 16:23:34 -0300

    Update profListar.php

--- GrowHealthy-desenvolvimento/profListar.php
+++ GrowHealthy-desenvolvimento/profListar.php
@@ -9,7 +9,7 @@
 
 <html>
 <head>
-    <title>IE - Instituição de Ensino</title>
+    <title>GrowHealthy</title>
     <link rel="icon" type="image/png" href="imagens/IE_favicon.png"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -54,58 +54,42 @@
 
 
                 // Faz Select na Base de Dados
-                $sql = "SELECT ID_Usuario, Nome, Celular, DataNasc, Login, Foto FROM TB_Usuario WHERE ID_TipoUsu = 2";
+                $sql = "SELECT CREF, Nome, celular, Email , login, dt_nasc FROM Personal";
                 echo "<div class='w3-responsive w3-card-4'>";
                 if ($result = $conn->query($sql)) {
                     echo "<table class='w3-table-all'>";
                     echo "	<tr>";
-                    echo "	  <th>Código</th>";
-                    echo "	  <th>Imagem</th>";
+                    echo "	  <th>CREF</th>";
                     echo "	  <th>Nome</th>";
                     echo "	  <th>Celular</th>";
-                    echo "	  <th>Data Nascimento</th>";
+                    echo "	  <th>Email</th>";
                     echo "	  <th>Login</th>";
+                    echo "	  <th>Data Nascimento</th>";                  
                     echo "	  <th> </th>";
                     echo "	  <th> </th>";
                     echo "	</tr>";
                     if ($result->num_rows > 0) {
                         // Apresenta cada linha da tabela
                         while ($row = $result->fetch_assoc() ) {
-                            $dataN = explode('-', $row["DataNasc"]);
+                            $dataN = explode('-', $row["dt_nasc"]);
                             $ano = $dataN[0];
                             $mes = $dataN[1];
                             $dia = $dataN[2];
-                            $cod = $row["ID_Usuario"];
                             $nova_data = $dia . '/' . $mes . '/' . $ano;
                             echo "<tr>";
-                            echo "<td>";
-                            echo $cod;
-                            echo "</td>";
-                            if ($row['Foto']) {?>
-                                <td>
-                                    <img id="imagemSelecionada" class="w3-circle w3-margin-top" src="data:image/png;base64,<?= base64_encode($row['Foto']) ?>" />
-                                </td><td>
-                                <?php
-                            } else {
-                                ?>
-                                <td>
-                                    <img id="imagemSelecionada" class="w3-circle w3-margin-top" src="imagens/pessoa.jpg" />
-                                </td><td>
-                                <?php
-                            }
                             echo $row["Nome"];
                             echo "</td><td>";
                             echo $row["Celular"];
                             echo "</td><td>";
                             echo $nova_data;
                             echo "</td><td>";
-                            echo $row["Login"];
+                            echo $row["login"];
                             echo "</td><td>";
-                            //Atualizar e Excluir registro de prof
+                            //Atualizar e Excluir registro de personal
             ?>                      
-                            <a href='profAtualizar.php?id=<?php echo $cod; ?>'><img src='imagens/Edit.png' title='Editar Professor' width='32'></a>
+                            <a href='profAtualizar.php'><img src='imagens/Edit.png' title='Editar Personal' width='32'></a>
                             </td><td>
-                            <a href='profExcluir.php?id=<?php echo $cod; ?>'><img src='imagens/Delete.png' title='Excluir Professor' width='32'></a>
+                            <a href='profExcluir.php'><img src='imagens/Delete.png' title='Excluir Personal' width='32'></a>
                             </td>
                             </tr>
             <?php
