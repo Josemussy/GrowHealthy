01b9c7969fc28a9d42647bfc6b31c17b414fc0a6
Felipe <107360437+felipesphair@users.noreply.github.com>
2023-06-09 00:22:06 -0300

    Add files via upload

--- repaginado/personal/alunoListar.php
+++ repaginado/personal/alunoListar.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>GrowHealthy</title>
-    <link rel="icon" type="image/png" href="imagens/logo1.png"/>
+    <link rel="icon" type="image/png" href="../imagens/logo1.png"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="../css/customize.css">
--- repaginado/personal/treinoAtualizar_exe.php
+++ repaginado/personal/treinoAtualizar_exe.php
@@ -3,7 +3,7 @@
 	<head>
 	  <meta charset="UTF-8">
 	  <title>GrowHealthy</title>
-	  <link rel="icon" type="image/png" href="imagens/IE_favicon.png" />
+	  <link rel="icon" type="image/png" href="../imagens/logo1.png" />
 	  <meta name="viewport" content="width=device-width, initial-scale=1">
 	  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	  <link rel="stylesheet" href="../css/customize.css">
